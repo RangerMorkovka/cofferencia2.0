c1ad6a570612e8914d52634df9584a7098ce7403
RangerMorkovka <artka3003@gmail.com>
2026-07-15 12:06:16 +1000

    db connecting

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,28 @@
-import {Pool} from 'pg';
+import { Pool } from 'pg';
 
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      // Этот блок сработает ТОЛЬКО на сервере Render
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false } 
+    })
+  : new Pool({
+      // Этот блок продолжит работать на вашем КОМПЬЮТЕРЕ (все ваши данные остаются без изменений)
+      user: 'postgres',
+      host: 'localhost',
+      database: 'cofferencia',
+      password: '123098qwe',
+      port: '5432',
+    });
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'cofferencia',
-    password: '123098qwe',
-    port: '5432',
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Ошибка подключения:', err);
+  } else {
+    console.log(process.env.DATABASE_URL ? 'Успешное подключение к облаку Render!' : 'Успешное подключение локально!');
+  }
 });
-pool.query('SELECT NOW()', (err,res) => {
-    if(err){
-        console.error('Ошибка', err);
-       
-    }else {
-        console.log('Успешное подключение');
-    }
-})
-export const db =  {
-    query: (text, params) => pool.query(text, params),
+
+export const db = {
+  query: (text, params) => pool.query(text, params),
 };
