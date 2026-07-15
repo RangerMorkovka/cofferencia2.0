a85a8fbeec196ef3f7d718d8a2eaa954ce2266f9
RangerMorkovka <artka3003@gmail.com>
2026-07-15 14:25:57 +1000

    ips mobile

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.set("trust proxy", true);
 
-const ALLOWED_IPS = ["178.176.69.197"];
+const ALLOWED_IPS = ["178.178.214.22", "178.176.69.243" ];
 
 app.get("/check-access", (req, res) => {
   const clientIP =
