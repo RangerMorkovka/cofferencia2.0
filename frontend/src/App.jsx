23fe0b8c5295c70c4ceb4b27981a73abc03855bd
RangerMorkovka <artka3003@gmail.com>
2026-08-04 18:40:00 +1000

    поправила пути к шрифтам и картинке хедера

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import {Login} from "./pages/LogIn/index.jsx";
+import {ChangePasswordForm} from "./pages/ChangePasswordForm/ChangePasswordForm.jsx"
+import { Home } from "./pages/Home.jsx";
+import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth.js";
+import { AddProduct } from "./pages/AddProduct/index.jsx";
+import { Admin } from "./pages/Admin/index.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import {ProtectedRoute} from './pages/ProtectedRoute.jsx';
+import './App.css';
+
+function App() {
+  
+  return (
+    <>
+     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/AddProduct" element={<AddProduct />} />
+        <Route path="/admin" element={
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>} />
+        <Route path="/addproduct/:id" element={<AddProduct />} />
+         <Route path="/changepasswordform" element={<ChangePasswordForm />} />
+
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/redux/slices/products.js
@@ -0,0 +1,93 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios, { instance } from "../../Axios";
+
+
+export const fetchProducts = createAsyncThunk(
+  "/products/fetchProducts",
+  async () => {
+    const { data } = await axios.get("api/products");
+    return data;
+  },
+);
+
+export const fetchProductVariants = createAsyncThunk(
+  "/product_variants/fetchProductVariants",
+  async () => {
+    const { data } = await axios.get("api/product_variants");
+    return data;
+  },
+);
+export const fetchEditProduct = createAsyncThunk('api/products/fetchEditProduct', async (id) => {
+    const {data} = await instance.get(`api/products/${id}`);
+    
+    return data;
+});
+export const fetchRemoveProducts = createAsyncThunk('/products/fetchRemoveProducts', async (id) => {
+    const {data} = await instance.delete(`api/products/${id}`);
+    return data;
+});
+const initialState = {
+  products: {
+    items: [],
+    status: "loading",
+  },
+  variants: {
+    items: [],
+    status: "loading",
+  },
+  edits: {
+    items: [],
+    status: "loading",
+  }
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducer: {},
+  extraReducers: {
+    [fetchProducts.pending]: (state) => {
+      state.products.items = [];
+      state.products.status = "loading";
+    },
+    [fetchProducts.fulfilled]: (state, action) => {
+      state.products.items = action.payload;
+      state.products.status = "loaded";
+    },
+    [fetchProducts.rejected]: (state) => {
+      state.products.items = [];
+      state.products.status = "error";
+    },
+
+    [fetchProductVariants.pending]: (state) => {
+      state.variants.items = [];
+      state.variants.status = "loading";
+    },
+    [fetchProductVariants.fulfilled]: (state, action) => {
+      state.variants.items = action.payload;
+      state.variants.status = "loaded";
+    },
+    [fetchProductVariants.rejected]: (state) => {
+      state.variants.items = [];
+      state.variants.status = "error";
+    },
+     [fetchEditProduct.rejected]: (state) => {
+      state.edits.items = [];
+      state.edits.status = "error";
+    },
+
+    [fetchEditProduct.pending]: (state) => {
+      state.edits.items = [];
+      state.edits.status = "loading";
+    },
+    [fetchEditProduct.fulfilled]: (state, action) => {
+      state.edits.items = action.payload;
+      state.edits.status = "loaded";
+    },
+     [fetchRemoveProducts.pending]: (state, action) => {
+            state.products.items = state.products.items.filter((obj) => obj.id !== action.meta.arg)
+        },
+  },
+});
+
+export const productsReducer = productsSlice.reducer;
