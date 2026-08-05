81e3dfe66f8cc8d75c665d3cf649f069672b2cf8
RangerMorkovka <artka3003@gmail.com>
2026-08-05 12:47:31 +1000

    delete frontend

--- frontend/src/components/index.jsx
+++
@@ -1,7 +0,0 @@
-export { Header } from "./Header/header";
-export { Location } from "./Location/location";
-export { Categories } from "./Categories/Categories";
-export { ProductCard } from "./ProductCard/productCard";
-export { AdminHeader } from "./AdminHeader/AdminHeader";
-export { AdminRow } from "./AdminRow/AdminRow";
-export { Footer } from "./Footer/Footer"; 
