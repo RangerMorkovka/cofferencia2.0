cd85d79b07df3fdd8550fc8725b82f01e8f578d6
RangerMorkovka <artka3003@gmail.com>
2026-08-05 14:56:02 +1000

    vercel.json vite.config.js rewrite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, "dist"),
+    outDir:  "dist",
     emptyOutDir: true,
   },
   server: {
