1fff6ec1d420d5ee22145aed01e42f4e893026a7
RangerMorkovka <artka3003@gmail.com>
2026-08-08 15:23:39 +1000

    удаление frontend,поправила gitignore, dist в bsckend

---
+++ backend/index.js
@@ -0,0 +1,105 @@
+import express from "express";
+import { put } from "@vercel/blob";
+import fs from "fs";
+import multer from "multer";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import pg from "pg";
+import { db } from "./config/db.js";
+import { loginValidation } from "./validation.js";
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
+import { UserController } from "./controllers/index.js";
+import { ProductController } from "./controllers/index.js";
+import { CategoriesController } from "./controllers/index.js";
+import { ProductVariantsController } from "./controllers/index.js";
+import "dotenv/config";
+import { error } from "console";
+const app = express();
+
+const port = process.env.port || 5174;
+app.use(cors());
+app.use(express.json());
+app.set("trust proxy", true);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+async function findUserByUserName(username) {
+  const queryText = "SELECT * FROM users WHERE username = $1;";
+  const result = await db.query(queryText, [username]);
+
+  if (result.rows.length === 0) {
+    return null;
+  }
+
+  return result.rows[0];
+}
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync("uploads/images")) {
+      fs.mkdirSync("uploads/images");
+    }
+    cb(null, "uploads/images");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+
+
+app.use("/uploads/images", express.static(path.join(__dirname, "uploads/images")));
+
+
+app.post(
+  "/api/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login,
+);
+app.patch("/api/auth/changepassword", checkAuth, UserController.changePassword);
+app.get("/api/auth/me", checkAuth, UserController.getMe);
+
+app.post(
+  "/api/uploads/images",
+  checkAuth,
+  upload.single("image"),
+  async (req, res) => {
+    
+   
+      
+    res.json({
+      img_url: `/uploads/images/${req.file.originalname}`,
+    });
+  
+});
+
+app.get("/api/categories", CategoriesController.getAllCategories);
+
+app.get("/api/products", ProductController.getAllProducts);
+app.get(
+  "/api/product_variants",
+  ProductVariantsController.getAllProductVariants,
+);
+app.get("/api/product_variants/:id", ProductVariantsController.getOne);
+//app.get('/posts/tags', PostController.getLastTags);
+app.get("/api/products/:id", ProductController.getOne);
+app.post("/api/products", ProductController.create);
+app.delete("/api/products/:id", checkAuth, ProductController.remove);
+app.patch("/api/products/:id", ProductController.update);
+
+app.patch("/api/product_variants/:id", ProductController.update);
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("/:page", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+app.listen(5174, "0.0.0.0", (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
